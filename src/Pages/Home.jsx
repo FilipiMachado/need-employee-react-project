76391a6650e22e61e3e9ad560002fdefb9e91837
filipi <filipichip7@gmail.com>
2023-05-06 17:07:48 -0300

    structuring user auth redirection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,17 @@ import { useEffect } from "react";
 
 import HomeComponent from "../components/HomeComponent";
 import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../firebaseConfig";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     onAuthStateChanged(auth, (res) => {
-      console.log(res?.accessToken);
+      if (!res?.accessToken) {
+        navigate("/");
+      }
     });
   }, []);
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,13 +2,18 @@ import { useEffect } from "react";
 
 import LoginComponent from "../components/LoginComponent";
 import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../firebaseConfig";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     onAuthStateChanged(auth, (res) => {
-      console.log(res?.accessToken);
+      if (res?.accessToken) {
+        navigate("/home");
+      }
     });
-  }, []);
+  }, [navigate]);
   return <LoginComponent />;
 }
