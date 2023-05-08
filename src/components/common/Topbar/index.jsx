7b94644a62fcf226fcb97205fbe6bbce0757d656
filipi <filipichip7@gmail.com>
2023-05-08 15:48:24 -0300

    structuring topbar

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,7 +1,22 @@
-import './index.scss'
+import {
+  AiOutlineHome,
+  AiOutlineUserSwitch,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+
+import "./index.scss";
+import NployeeLogo from "../../../assets/main-logo.png";
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <div className="topbar-main">
+      <img className="main-logo" src={NployeeLogo} alt="Main Logo" />
+      <div className="icons-wrapper">
+        <AiOutlineHome size={25} />
+        <AiOutlineUserSwitch size={25} />
+        <BsFillBriefcaseFill size={25} />
+      </div>
+    </div>
+  );
 }
