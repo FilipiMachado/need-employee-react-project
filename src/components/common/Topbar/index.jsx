a7b3a5581caf059a18ea49f6862971a1e0ace44e
filipi <filipichip7@gmail.com>
2023-05-08 14:52:01 -0300

    working on topbar

---
+++ src/components/common/Topbar/index.jsx
@@ -0,0 +1,7 @@
+import './index.scss'
+
+export default function Topbar() {
+  return (
+    <div>Topbar</div>
+  )
+}
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import Home from "../Pages/Home";
+import Topbar from "../components/common/Topbar";
+
+export default function HomeLayout() {
+  return (
+    <div>
+      <Topbar />
+      <Home />
+    </div>
+  );
+}
